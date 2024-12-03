c9806f99e592d63c1b14fe9916669fa9e6cc2906
Marcus Ilgner <mail@marcusilgner.com>
2024-12-03 20:32:06 +0100

    feat: Day 03 Part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,5 +1,6 @@
 
 val mulInstruction = """mul\((\d+),(\d+)\)""".toRegex()
+val mulWithDoOrDont = """(do(?:n't)?\(\))|(?:mul\((\d+),(\d+)\))""".toRegex()
 
 fun main() {
     val input = readInput("day03_input").joinToString()
@@ -10,5 +11,20 @@ fun main() {
         }
         .sum()
 
+    var doIt = true
+    val totalDoOrDont = mulWithDoOrDont.findAll(input)
+        .map {
+            val (doOrDont, f1, f2) = it.destructured
+            if (!doOrDont.isNullOrBlank()) {
+                doIt = doOrDont == "do()"
+                0
+            } else if (doIt) {
+                f1.toInt() * f2.toInt()
+            } else {
+                0
+            }
+        }.sum()
+
     println("Total: $total")
+    println("DoOrDont: $totalDoOrDont")
 }
