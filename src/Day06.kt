ed3c939b53747ba43cedd2648af480599e70e323
Marcus Ilgner <mail@marcusilgner.com>
2024-12-10 19:33:36 +0100

    feat: Day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,64 +1,135 @@
 enum class GuardDirection(val char: Char) {
     // guards turn by 90 degree, in the direction of the enum
-    UP('^'), RIGHT('>'), DOWN('V'), LEFT('<'),
+    UP('^'), RIGHT('>'), DOWN('V'), LEFT('<');
+
+    fun turn(): GuardDirection = entries[(entries.indexOf(this) + 1) % GuardDirection.entries.size]
 }
 
-fun main() {
-    val mapArray = readInput("day06_input").map { it.toCharArray() }.toTypedArray()
-    val mapWidth = mapArray[0].size
+data class Map(val grid: Array<CharArray>) {
+    lateinit var guardPosition: Pair<Int, Int>
+    lateinit var guardDirection: GuardDirection
 
-    var guardPos: Pair<Int, Int>? = null
+    val width = grid[0].size
+    val height = grid.size
 
-    fun Pair<Int, Int>.outOfBounds() = first < 0 || first == mapWidth || second < 0 || second == mapArray.size
+    init {
+        grid.forEachIndexed findPos@{ y, line ->
+            line.forEachIndexed { x, c ->
+                GuardDirection.entries.forEach { direction ->
+                    if (c == direction.char) {
+                        guardPosition = Pair(x, y)
+                        guardDirection = direction
+                        markGrid(guardPosition, '.')
+                        return@findPos
+                    }
+                }
+            }
+        }
+        markGrid(guardPosition, '.')
+    }
 
-    var guardDirection: GuardDirection? = null
-    mapArray.forEachIndexed findPos@{ y, line ->
-        line.forEachIndexed { x, c ->
-            GuardDirection.entries.forEach { direction ->
-                if (c == direction.char) {
-                    guardPos = Pair(x, y)
-                    guardDirection = direction
-                    return@findPos
+    val fieldAhead get() = fieldInDirection(guardDirection)
+
+    fun withChangedField(position: Pair<Int, Int>, change: Char, block: () -> Unit) {
+        val oldPos = guardPosition
+        val oldDirection = guardDirection
+        val currentField = grid[position.second][position.first]
+
+        markGrid(position, change)
+        block()
+        this.guardPosition = oldPos
+        this.guardDirection = oldDirection
+        markGrid(position, currentField)
+    }
+
+    fun fieldInDirection(direction: GuardDirection): Pair<Int, Int> =
+        when (direction) {
+            GuardDirection.RIGHT -> {
+                guardPosition.first + 1 to guardPosition.second
+            }
+
+            GuardDirection.UP -> {
+                guardPosition.first to guardPosition.second - 1
+            }
+
+            GuardDirection.DOWN -> {
+                guardPosition.first to guardPosition.second + 1
+            }
+
+            GuardDirection.LEFT -> {
+                guardPosition.first - 1 to guardPosition.second
+            }
+        }
+
+    // walk one step and return the new position
+    fun step(): Boolean {
+        while (true) {
+            val next = fieldAhead
+            if (outOfBounds(next)) {
+                return false
+            }
+            if (grid[next.second][next.first] == '#') {
+                guardDirection = guardDirection.turn()
+                continue
+            } else {
+                guardPosition = next
+                return true
+            }
+        }
+    }
+
+    fun outOfBounds(pos: Pair<Int, Int>) = pos.first < 0 || pos.first == width || pos.second < 0 || pos.second == height
+
+    fun markGrid(pos: Pair<Int, Int>, c: Char) {
+        grid[pos.second][pos.first] = c
+    }
+}
+
+fun loadMapFromInput() = Map(readInput("day06_input").map { it.toCharArray() }.toTypedArray())
+
+fun checkTrajectory() {
+    val map = loadMapFromInput()
+    do {
+        map.markGrid(map.guardPosition, 'X')
+    } while (map.step())
+
+    val totalPassed = map.grid.sumOf { line -> line.count { it == 'X' } }
+    println("Guard passed $totalPassed fields")
+}
+
+fun checkInfiniteLoops() {
+    val map1 = loadMapFromInput()
+    val map2 = loadMapFromInput()
+
+    var obstacles = 0;
+
+    for (x in 0..<map1.width) {
+        for (y in 0..<map1.height) {
+            if (map1.grid[y][x] == '#') {
+                continue
+            }
+            map1.withChangedField(x to y, '#') {
+                map2.withChangedField(x to y, '#') {
+                    // with guard on map 1 travelling twice as fast as the guard on map 2,
+                    // if they ever meet on the same field while facing in the same direction,
+                    // there is an infinite loop
+                    // but if the guard leaves map 1, there is no infinite loop
+                    while (map1.step() && map1.step()) {
+                        map2.step()
+                        if (map1.guardPosition == map2.guardPosition && map1.guardDirection == map2.guardDirection) {
+                            obstacles++
+                            break
+                        }
+                    }
                 }
             }
         }
     }
 
-    while (true) {
-        val newGuardPos = when (guardDirection) {
-            GuardDirection.RIGHT -> {
-                guardPos!!.first + 1 to guardPos!!.second
-            }
+    println("Managed to set $obstacles obstacles for infinite loops")
+}
 
-            GuardDirection.UP -> {
-                guardPos!!.first to guardPos!!.second - 1
-            }
-
-            GuardDirection.DOWN -> {
-                guardPos!!.first to guardPos!!.second + 1
-            }
-
-            GuardDirection.LEFT -> {
-                guardPos!!.first - 1 to guardPos!!.second
-            }
-
-            null -> null
-        }
-        if (newGuardPos == null || newGuardPos.outOfBounds()) {
-            mapArray[guardPos!!.second][guardPos!!.first] = 'X'
-            break
-        }
-        if (mapArray[newGuardPos.second][newGuardPos.first] == '#') {
-            // need to turn
-            val idx = GuardDirection.entries.indexOf(guardDirection)
-            guardDirection = GuardDirection.entries[(idx + 1) % GuardDirection.entries.size]
-        } else {
-            mapArray[guardPos!!.second][guardPos!!.first] = 'X'
-            mapArray[newGuardPos.second][newGuardPos.first] = guardDirection!!.char
-            guardPos = newGuardPos
-        }
-    }
-
-    val totalPassed = mapArray.sumOf { line -> line.count { it == 'X' } }
-    println("Guard passed $totalPassed fields")
+fun main() {
+    checkTrajectory()
+    checkInfiniteLoops()
 }
